c5407dbd6b645b2a0406660ef9d16515f97e9df4
andrew <andry925@ukr.net>
2024-02-08 12:02:26 +0200

    Adding UserLogin endoint

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import login
+from rest_framework.authentication import SessionAuthentication
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions, status
-from .serializers import UserSerializer
+from .serializers import UserSerializer, UserLoginSerializer
 
 
 class UserRegister(APIView):
@@ -16,3 +18,16 @@ class UserRegister(APIView):
                     serializer.data,
                     status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLogin(APIView):
+    permission_classes = (permissions.AllowAny,)
+    authentication_classes = (SessionAuthentication,)
+
+    def post(self, request):
+        data = request.data
+        serializer = UserLoginSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.login_user(data)
+            login(request, user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
