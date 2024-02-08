030eac0005b2cd7305a68d75470c84f869b88285
Anastasiia Snihur <127547279+ansnigur@users.noreply.github.com>
2024-02-08 16:50:47 +0200

    Feature-frontend (#6)
    * LoginSignup form created
    
    * LoginSignup page created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import PageLoginSignup from './Components/PageLoginSignup/PageLoginSignup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PageLoginSignup/>
     </div>
   );
 }
