965257f1fd7c3684a87957eb7da30ae7d208e5dd
Anastasiia Snihur <127547279+ansnigur@users.noreply.github.com>
2024-02-09 00:45:21 +0200

    Routing added & Folders management (#7)
    * LoginSignup form created
    
    * LoginSignup page created
    
    * Routing established for Signup & Login pages / Folders management

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
-import PageLoginSignup from './Components/PageLoginSignup/PageLoginSignup';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// import NotLoggedIn from './Components/PageHome/NotLoggedIn'
+import Home from './Pages/Home';
+import LoginPage from './Pages/LoginPage';
+import Signup from './Components/Signup/Signup';
 
 function App() {
   return (
     <div>
-      <PageLoginSignup/>
+      <Home />
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
 import App from './App';
+import LoginPage from './Pages/LoginPage'
+import SignupPage from "./Pages/SignupPage"
+
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "signup",
+    element: <SignupPage/>,
+  },
+  {
+    path: "login",
+    element: <LoginPage/>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>
 );
 
