670efadf6f6f98ca4455efbcb8afd9b658279676
Anastasiia Snihur <nastenasnigur@gmail.com>
2024-02-10 23:52:14 +0200

    Fixed Login

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import "./Login.css"
-import { useState } from 'react'
-import axios from 'axios'
-import envelope from "../Assets/envelope.svg"
-import lock from "../Assets/lock.svg"
+import React, { useState } from 'react';
+import axios from 'axios';
+import envelope from "../Assets/envelope.svg";
+import lock from "../Assets/lock.svg";
+import { useNavigate } from 'react-router-dom';
 
-axios.defaults.withCredentials = true
+axios.defaults.withCredentials = true;
 
 const Login = ({ setCurrentUser }) => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const Login = async () => {
+  const handleLogin = async () => {
     try {
       const response = await axios.post("http://127.0.0.1:8000/accounts/login", {
         email,
         password,
-      }, { withCredentials: true })
+      }, { withCredentials: true });
 
-      if (response.status === 200 || response.status === 201) {
-        setCurrentUser(true)
+      if (response.status === 201 || response.status === 200) {
+        setCurrentUser(true);
+        navigate('/home-acc');
       }
     } catch (error) {
-      console.error("Such user does not exist", error)
+      console.error("Such user does not exist", error);
     }
   }
+
   return (
     <div className='container'>
         <div className="header">
@@ -42,12 +44,10 @@ const Login = ({ setCurrentUser }) => {
             </div>
         </div>
       <div className="submit-container">
-        <a href='/home-acc' className='submit'>
           <button
             className="submit"
-            onClick={Login}>Увійти
+            onClick={handleLogin}>Увійти
           </button>
-        </a>
       </div>
     </div>
   )
