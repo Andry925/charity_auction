3179a01cfc931fc0d71f341092a0a9af22069486
andrew <andry925@ukr.net>
2024-02-10 11:51:40 +0200

    Configuring html with code

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -2,57 +2,58 @@ import React from 'react'
 import "./Login.css"
 import { useState } from 'react'
 import axios from 'axios'
-import user from "../Assets/user.svg"
 import envelope from "../Assets/envelope.svg"
 import lock from "../Assets/lock.svg"
 
 axios.defaults.withCredentials = true
 
 const Login = ({ setCurrentUser }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-  const handleLogin = async () => {
+  const Login = async () => {
     try {
       const response = await axios.post("http://127.0.0.1:8000/accounts/login", {
         email,
         password,
-      }, { withCredentials: true });
+      }, { withCredentials: true })
 
       if (response.status === 200 || response.status === 201) {
-        setCurrentUser(true);
+        setCurrentUser(true)
       }
     } catch (error) {
-      console.error("Such user does not exist", error);
+      console.error("Such user does not exist", error)
     }
-  };
+  }
   return (
     <div className='container'>
         <div className="header">
-            <div className="text">{action}</div>
+            <div className="text">Увійти</div>
             <div className="underline"></div>
         </div>
         <div className="inputs">
-        {action === "Увійти" ? <div></div>:
-                <div className="input">
-                <img src={user} alt="user" />
-                <input type="text" placeholder="Ім'я"/>
-                </div>}
             <div className="input">
-                <img src={envelope} alt="email" />
-                <input type="email" placeholder='E-mail'/>
+                <img src={envelope} alt="email"/>
+                <input type="email" placeholder='E-mail' onChange={(e) => setEmail(e.target.value)}/>
             </div>
             <div className="input">
-                <img src={lock} alt="lock" />
-                <input type="password" placeholder='Пароль'/>
+                <img src={lock} alt="lock"/>
+                <input type="password" placeholder='Пароль' onChange={(e) => setPassword(e.target.value)}/>
             </div>
         </div>
-        <div className="submit-container">
-            <div className={action === "Увійти" ? "submit yellow" : "submit"} onClick={() => {setAction("Реєстрація")}}>Реєстрація</div>
-            <div className={action === "Реєстрація" ? "submit yellow" : "submit"} onClick={() => {setAction("Увійти")}}>Увійти</div>
+      <div className="submit-container">
+        <div
+          className="submit yellow"
+          onClick={Login}>Увійти
         </div>
+        <div
+          className="submit yellow"
+          onClick={() => {
+          }}>Реєстрація
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Login;
+export default Login
