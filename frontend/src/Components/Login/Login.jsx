c6fbe272531f938e6ad2d173d43a5b52cfd31e4f
andrew <andry925@ukr.net>
2024-02-10 11:35:14 +0200

    Adding login functionality on frontend

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import "./Login.css"
 import { useState } from 'react'
-
+import axios from 'axios'
 import user from "../Assets/user.svg"
 import envelope from "../Assets/envelope.svg"
 import lock from "../Assets/lock.svg"
 
+axios.defaults.withCredentials = true
 
-const Login = () => { 
-        const [action, setAction] = useState("Увійти");
+const Login = ({ setCurrentUser }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post("http://127.0.0.1:8000/accounts/login", {
+        email,
+        password,
+      }, { withCredentials: true });
+
+      if (response.status === 200 || response.status === 201) {
+        setCurrentUser(true);
+      }
+    } catch (error) {
+      console.error("Such user does not exist", error);
+    }
+  };
   return (
     <div className='container'>
         <div className="header">
