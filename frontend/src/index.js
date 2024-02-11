95aa5778fc2a1b82a280b9a06949b9ac174637b5
andrew <andry925@ukr.net>
2024-02-11 16:49:00 +0200

    Adding final routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
 import App from './App';
 import Home from './Pages/Home'
 import HomeAccount from './Pages/HomeAccount'
@@ -9,54 +8,72 @@ import LoginPage from './Pages/LoginPage'
 import SignupPage from "./Pages/SignupPage"
 import CabinetPage from './Pages/CabinetPage';
 import AllAuctions from './Pages/AllAuctions';
-
 import reportWebVitals from './reportWebVitals';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 
-
+import PrivateRoute from "./PrivateRoute";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App/>,
+    path: '/',
+    element: <App />,
   },
   {
-    path: "/home",
-    element: <Home/>,
+    path: '/home',
+    element: <Home />,
   },
   {
-    path: "/home-acc",
-    element: <HomeAccount/>,
+    path: '/home-acc',
+    element: (
+      <PrivateRoute
+        element={<HomeAccount />}
+        isAuthenticated={PrivateRoute.isAuthenticated}
+        redirectTo="/login"
+      />
+    ),
   },
   {
-    path: "/signup",
-    element: <SignupPage/>,
+    path: '/signup',
+    element: <SignupPage />,
   },
   {
-    path: "/login",
-    element: <LoginPage/>,
+    path: '/login',
+    element: <LoginPage />,
   },
   {
-    path: "/cabinet",
-    element: <CabinetPage/>,
+    path: '/cabinet',
+    element: (
+      <PrivateRoute
+        element={<CabinetPage />}
+        isAuthenticated={PrivateRoute.isAuthenticated}
+        redirectTo="/login"
+      />
+    ),
   },
   {
-    path: "/all-auctions",
-    element: <AllAuctions/>,
+    path: '/all-auctions',
+    element: (
+      <PrivateRoute
+        element={<AllAuctions />}
+        isAuthenticated={PrivateRoute.isAuthenticated}
+        redirectTo="/login"
+      />
+    ),
   },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
